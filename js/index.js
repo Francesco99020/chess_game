a87719fc4687aeb11780bc94d001790cdd5965fc
Francesco D'Arrigo <francescoda1234@gmail.com>
2022-06-12 22:30:08 -0400

    Added layout for rulesets to be defined

---
+++ js/index.js
@@ -0,0 +1,76 @@
+/*Make an array of objects for each piece to count amount times the piece
+has moved(for pawns). And to store ruleset for how each piece can
+move on the board.
+*/
+function checkTile(id){
+    var element = document.getElementById(id).children;
+    console.log(element[0].id);
+    getRuleSet(element[0].id);
+}
+function getRuleSet(element){
+    if(element == 'white king'){
+        
+    }else if(element == 'white-queen'){
+
+    }else if(element == 'white-bishop-1'){
+        
+    }else if(element == 'white-bishop-2'){
+        
+    }else if(element == 'white-horse-1'){
+        
+    }else if(element == 'white-horse-2'){
+        
+    }else if(element == 'white-rook-1'){
+        
+    }else if(element == 'white-rook-2'){
+        
+    }else if(element == 'white-pawn-1'){
+        
+    }else if(element == 'white-pawn-2'){
+        
+    }else if(element == 'white-pawn-3'){
+        
+    }else if(element == 'white-pawn-4'){
+        
+    }else if(element == 'white-pawn-5'){
+        
+    }else if(element == 'white-pawn-6'){
+        
+    }else if(element == 'white-pawn-7'){
+        
+    }else if(element == 'white-pawn-8'){
+        
+    }else if(element == 'black king'){
+        
+    }else if(element == 'black-queen'){
+
+    }else if(element == 'black-bishop-1'){
+        
+    }else if(element == 'black-bishop-2'){
+        
+    }else if(element == 'black-horse-1'){
+        
+    }else if(element == 'black-horse-2'){
+        
+    }else if(element == 'black-rook-1'){
+        
+    }else if(element == 'black-rook-2'){
+        
+    }else if(element == 'black-pawn-1'){
+        
+    }else if(element == 'black-pawn-2'){
+        
+    }else if(element == 'black-pawn-3'){
+        
+    }else if(element == 'black-pawn-4'){
+        
+    }else if(element == 'black-pawn-5'){
+        
+    }else if(element == 'black-pawn-6'){
+        
+    }else if(element == 'black-pawn-7'){
+        
+    }else if(element == 'black-pawn-8'){
+        
+    }
+}
